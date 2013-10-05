40569afe6e71541a95248a4dc42578be865137e9
Gerton ten Ham <gerton.tenham@gmail.com>
2013-10-05 15:42:17 +0200

    Moved loading of Spring application context loader to separate artifact. - Uses package scan for Spring Configuration classes

--- magno-rest/src/main/java/nl/magnomodulus/application/config/RestApplicationConfig.java
+++ magno-rest/src/main/java/nl/magnomodulus/application/config/RestApplicationConfig.java
@@ -1,10 +1,10 @@
-package nl.magnomodulus.rest.config.server;
+package nl.magnomodulus.application.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan(basePackages = {"nl.magnomodulus.rest.service"})
-public class WebApplicationConfig {
+public class RestApplicationConfig {
 
 }
